39aa883f23d053f6007fcafba59500fa4b7e9ef4
ithkkeii <ithkkeii@gmail.com>
2020-05-08 15:49:18 +0700

    new webpack setting

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,20 +1,65 @@
+const path = require("path");
+const webpack = require("webpack");
+
+/*
+ * SplitChunksPlugin is enabled by default and replaced
+ * deprecated CommonsChunkPlugin. It automatically identifies modules which
+ * should be splitted of chunk by heuristics using module duplication count and
+ * module category (i. e. node_modules). And splits the chunks…
+ *
+ * It is safe to remove "splitChunks" from the generated configuration
+ * and was added as an educational example.
+ *
+ * https://webpack.js.org/plugins/split-chunks-plugin/
+ *
+ */
+
+/*
+ * We've enabled MiniCssExtractPlugin for you. This allows your app to
+ * use css modules that will be moved into a separate CSS file instead of inside
+ * one of your module entries!
+ *
+ * https://github.com/webpack-contrib/mini-css-extract-plugin
+ *
+ */
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+/*
+ * We've enabled TerserPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/terser-webpack-plugin
+ *
+ */
+
+const TerserPlugin = require("terser-webpack-plugin");
+
+const workboxPlugin = require("workbox-webpack-plugin");
+
 module.exports = {
-  mode: "production",
+  mode: "development",
+  // entry: {},
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  plugins: [
+    new webpack.ProgressPlugin(),
+    // new MiniCssExtractPlugin({ filename: "main.[chunkhash].css" }),
+    new MiniCssExtractPlugin(),
+    // new workboxPlugin.GenerateSW({
+    //   swDest: "sw.js",
+    //   clientsClaim: true,
+    //   skipWaiting: false,
+    // }),
+  ],
 
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx", ".js"],
-  },
+  node: { fs: "empty" },
 
   module: {
     rules: [
       {
-        // test: /\.ts(x?)$/,
+        // This regex may be wrong because it dont works
+        // test: /.(ts|tsx)?$/,
         test: /\.tsx?$/,
-        exclude: /node_modules/,
         use: [
           {
             loader: "ts-loader",
@@ -23,36 +68,66 @@ module.exports = {
             },
           },
         ],
+        include: [path.resolve(__dirname, "src")],
+        exclude: [/node_modules/],
       },
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
+        test: /.(scss|css)$/,
+
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
           },
-        },
+          // {
+          //   loader: "style-loader",
+          // },
+          {
+            loader: "css-loader",
+
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
       },
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader",
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+      // {
+      //   enforce: "pre",
+      //   test: /\.js$/,
+      //   loader: "source-map-loader",
+      // },
+      // {
+      //   test: /\.css$/i,
+      //   use: ["style-loader", "css-loader"],
+      // },
     ],
   },
 
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-    react: "React",
-    "react-dom": "ReactDOM",
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+
+  optimization: {
+    minimizer: [new TerserPlugin()],
+
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/,
+        },
+      },
+
+      chunks: "async",
+      minChunks: 1,
+      minSize: 30000,
+      name: true,
+    },
   },
 };
