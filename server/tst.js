5c0ac8a63fb3f360d4d6b45a5441b5671f413ec1
ithkkeii <ithkkeii@gmail.com>
2020-05-28 12:32:42 +0700

    fix '\n' problem in worker

--- server/tst.js
+++ server/tst.js
@@ -48,3 +48,53 @@ function main() {
 
   ws.end();
 }
+
+const { parentPort, workerData } = require("worker_threads");
+const fs = require("fs");
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .replace(/s*$/, "")
+    .split("\\n")
+    .map((str) => str.replace(/s*$/, ""));
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c) {
+  console.log(c);
+  let path = [];
+  c.forEach((cloud, index) => {
+    if (cloud === 0) path.push(index);
+  });
+
+  for (let i = 0; i < path.length - 2; i++) {
+    // Can jump
+    if (path[i + 2] - path[i] === 2) {
+      path.splice(i + 1, 1);
+    }
+  }
+
+  return path.length - 1;
+}
+
+function main() {
+  const c = readLine()
+    .readLine()
+    .split(" ")
+    .map((cTemp) => parseInt(cTemp, 10));
+  let result = jumpingOnClouds(c);
+  parentPort.postMessage(result);
+}
