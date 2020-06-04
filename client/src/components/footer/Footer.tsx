8e664b08aa35c011ebf64e6cf51249a38254f426
ithkkeii <ithkkeii@gmail.com>
2020-06-04 20:18:24 +0700

    add footer

---
+++ client/src/components/footer/Footer.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import { STheme } from "../../theme";
+
+const Footer: React.FC = (props) => {
+  return (
+    <SFooter>
+      <span>Created by @ithkkeii. Copyright©2020, All rights reserved.</span>
+    </SFooter>
+  );
+};
+
+const SFooter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 50px;
+  background: ${({ theme }: { theme: STheme }) =>
+    theme.palette.background.white};
+  color: ${({ theme }: { theme: STheme }) => theme.palette.common.darkBlue};
+
+  & > span {
+    cursor: pointer;
+  }
+`;
+
+export default Footer;
