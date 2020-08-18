f752f6095b19cd0140bb82381c7b6bbf73e06621
ithkkeii <ithkkeii@gmail.com>
2020-08-18 17:19:28 +0700

    :zap: Enable cors
    .

--- servernest/src/main.ts
+++ servernest/src/main.ts
@@ -6,6 +6,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  // Support library
+  app.enableCors();
+
   await app.listen(serverPort || 3000);
 }
 bootstrap();
