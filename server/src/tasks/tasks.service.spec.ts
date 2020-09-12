08e083213a5eaf9c9a3e389a942f9071d35a80b7
ithkkeii <ithkkeii@gmail.com>
2020-09-12 19:29:21 +0700

    :white_check_mark: Test for tasksService
    .

--- server/src/tasks/tasks.service.spec.ts
+++ server/src/tasks/tasks.service.spec.ts
@@ -1,18 +1,32 @@
+import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from '../config/config.service';
+import { UsersModule } from '../users/users.module';
 import { TasksService } from './tasks.service';
 
 describe('TasksService', () => {
-  let service: TasksService;
+  let app: INestApplication;
+  let tasksService: TasksService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot(configService.getTypeOrmTestConfig()),
+        UsersModule,
+      ],
       providers: [TasksService],
     }).compile();
 
-    service = module.get<TasksService>(TasksService);
+    tasksService = module.get<TasksService>(TasksService);
+
+    app = module.createNestApplication();
+    await app.init();
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(tasksService).toBeDefined();
   });
+
+  afterEach(async () => await app.close());
 });
