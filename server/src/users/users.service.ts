7edde23a31446cd8d3481e91f28e9ff17b6345d0
ithkkeii <ithkkeii@gmail.com>
2020-09-15 23:27:42 +0700

    :pencil2: Shity typo.
    .

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -215,7 +215,7 @@ export class UsersService {
    */
   async findInactiveAccounts(): Promise<User[]> {
     // a day ago
-    const limitTime = new Date(Date.now() > -1000 * 60 * 60 * 24);
+    const limitTime = new Date(Date.now() - 1000 * 60 * 60 * 24);
 
     return this.usersRepository
       .createQueryBuilder('user')
